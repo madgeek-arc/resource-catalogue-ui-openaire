9b9b7a2d06196967f448c19d1d601db19def7dba
Stefania Martziou <stefania.martziou@imis.athena-innovation.gr>
2021-09-01 12:58:37 +0300

    First draft for the resources&support submenu of the service landing page, and various css overridees

--- src/app/pages/landingpages/service/resourcesAndSupport/resourcesAndSupport.component.ts
+++ src/app/pages/landingpages/service/resourcesAndSupport/resourcesAndSupport.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-service-landing-page-pricing',
+  selector: 'app-service-landing-page-resources-and-support',
   templateUrl: 'resourcesAndSupport.component.html',
   styleUrls: ['../../landing-page.component.css']
 })
