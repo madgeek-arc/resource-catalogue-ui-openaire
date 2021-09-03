17ede1614792b568a52eb7c4359bf50835323a99
jBalasis <cs2180008@di.uoa.gr>
2021-09-03 14:00:40 +0300

    added the form values for landing miscellaneous page

--- src/app/pages/landingpages/service/miscellaneous/miscellaneous.component.ts
+++ src/app/pages/landingpages/service/miscellaneous/miscellaneous.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormArray, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-service-landing-page-misc',
@@ -11,6 +11,18 @@ export class MiscellaneousComponent implements OnInit {
   @Input() form: FormGroup;
 
   ngOnInit() {
+  }
+
+  getServiceField(field: string) {
+    return this.form.get('service.' + field) as FormControl;
+  }
+
+  getExtrasField(field: string) {
+    return this.form.get('extras.' + field) as FormControl;
+  }
+
+  navigateTo(url: string) {
+    window.open(url, "_blank");
   }
 
 }
