d4e57b3144fbf12cb9de0928ad40444adcde63de
Konstantinos Spyrou <spyroukon@gmail.com>
2023-03-27 14:16:53 +0300

    renamed PLATFORM_API_ENDPOINT to PROXY_API_ENDPOINT

--- init.sh
+++ init.sh
@@ -5,10 +5,10 @@ CONF_TMPL=/etc/nginx/nginx.conf.txt
 PROXY_CONF_FILE=/etc/nginx/conf.d/site.conf
 EMAIL_ARG="--register-unsafely-without-email"
 ADD_SERVER_NAME=""
-PROXY_API=""
+ADD_PROXY_API=""
 
 # do not forget to escape proxy vars (else envsubst removes them)
-read -r -d '' PROXY_API_CONF << EOM
+read -r -d '' ADD_PROXY_API_CONF << EOM
     location ^~ /api {
          proxy_set_header        Host \$host;
          proxy_set_header        X-Real-IP \$remote_addr;
@@ -29,8 +29,8 @@ else
     echo "Creating Nginx configuration: $PROXY_CONF_FILE"
 
     [ ! -z ${SERVER_NAME+x} ] && export ADD_SERVER_NAME="server_name ${SERVER_NAME}";
-    [ ! -z ${PROXY_API_ENDPOINT+x} ] && export PROXY_API=$(echo "$PROXY_API_CONF");
-    envsubst '${ADD_SERVER_NAME} ${PROXY_API}' < $CONF_TMPL > $PROXY_CONF_FILE
+    [ ! -z ${PROXY_API_ENDPOINT+x} ] && export ADD_PROXY_API=$(echo "$ADD_PROXY_API_CONF");
+    envsubst '${ADD_SERVER_NAME} ${ADD_PROXY_API}' < $CONF_TMPL > $PROXY_CONF_FILE
 
     rm /etc/nginx/conf.d/default.conf || echo "File '/etc/nginx/conf.d/default.conf' already deleted. OK"
     nginx -t
