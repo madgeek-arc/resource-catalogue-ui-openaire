a2da45daeae1265c9afee6922c7ec12e81e24a40
Konstantinos Spyrou <spyroukon@gmail.com>
2023-03-27 13:29:20 +0300

    conditional creation of /api proxy configuration when PROXY_API_ENDPOINT env exists

--- init.sh
+++ init.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-# Create Nginx configuration
+## Init vars
 CONF_TMPL=/etc/nginx/nginx.conf.txt
-FILE=/etc/nginx/conf.d/site.conf
+PROXY_CONF_FILE=/etc/nginx/conf.d/site.conf
 EMAIL_ARG="--register-unsafely-without-email"
 ADD_SERVER_NAME=""
+PROXY_API=""
 
-if [ -f "$FILE" ]; then
-    echo "Nginx configuration already exists: $FILE "
+# do not forget to escape proxy vars (else envsubst removes them)
+read -r -d '' PROXY_API_CONF << EOM
+    location ^~ /api {
+         proxy_set_header        Host \$host;
+         proxy_set_header        X-Real-IP \$remote_addr;
+         proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
+         proxy_set_header        X-Forwarded-Proto \$scheme;
+         proxy_pass              ${PROXY_API_ENDPOINT};
+         proxy_read_timeout      300;
+         proxy_send_timeout      300;
+         client_max_body_size    200M;
+    }
+EOM
+
+
+## Create Nginx configuration ##
+if [ -f "$PROXY_CONF_FILE" ]; then
+    echo "Nginx configuration already exists: $PROXY_CONF_FILE "
 else
-    echo "Creating Nginx configuration: $FILE"
+    echo "Creating Nginx configuration: $PROXY_CONF_FILE"
 
     [ ! -z ${SERVER_NAME+x} ] && export ADD_SERVER_NAME="server_name ${SERVER_NAME}";
-    envsubst '${ADD_SERVER_NAME} ${PLATFORM_API_ENDPOINT} ${STATS_API_ENDPOINT}' < $CONF_TMPL > $FILE
+    [ ! -z ${PROXY_API_ENDPOINT+x} ] && export PROXY_API=$(echo "$PROXY_API_CONF");
+    envsubst '${ADD_SERVER_NAME} ${PROXY_API}' < $CONF_TMPL > $PROXY_CONF_FILE
 
     rm /etc/nginx/conf.d/default.conf || echo "File '/etc/nginx/conf.d/default.conf' already deleted. OK"
     nginx -t
-    cat $FILE
+    cat $PROXY_CONF_FILE
     nginx -s reload
 fi
 
