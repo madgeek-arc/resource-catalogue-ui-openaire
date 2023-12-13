adcc97bc31b48a33bd6b4e6831f2e200d7137f94
amantas <andreas_mantas@hotmail.com>
2023-12-13 14:46:02 +0200

    fix provider info page data display

--- src/app/pages/provider/dashboard/provider-Info/provider-info.component.ts
+++ src/app/pages/provider/dashboard/provider-Info/provider-info.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Provider, ProviderBundle, Vocabulary} from '../../../../entities/eic-model';
+import {Provider, ProviderBundle, Type, Vocabulary} from '../../../../entities/eic-model';
 import {ProviderService} from '../../../../services/provider.service';
 import {ResourceService} from '../../../../services/resource.service';
 
@@ -15,13 +15,29 @@ export class ProviderInfoComponent implements OnInit, OnChanges {
   provider: Provider = null
   vocabularies: Map<string, Object[]> = null;
 
+  // vocabularies: Map<string, Vocabulary[]> = null;
+  // placesVocabulary: Vocabulary[] = null;
+  // providerTypeVocabulary: Vocabulary[] = null;
+  // providerLCSVocabulary: Vocabulary[] = null;
+  // domainsVocabulary: Vocabulary[] = null;
+  // categoriesVocabulary: Vocabulary[] = null;
+  // merilDomainsVocabulary: Vocabulary[] = null;
+  // merilCategoriesVocabulary: Vocabulary[] = null;
+  // esfriDomainVocabulary: Vocabulary[] = null;
+  // legalStatusVocabulary: Vocabulary[] = null;
+  // esfriVocabulary: Vocabulary[] = null;
+  // areasOfActivityVocabulary: Vocabulary[] = null;
+  // networksVocabulary: Vocabulary[] = null;
+  // societalGrandChallengesVocabulary: Vocabulary[] = null;
+  // hostingLegalEntityVocabulary: Vocabulary[] = null;
+
   constructor(private resourceService: ResourceService) {
   }
 
   ngOnInit() {
-    this.resourceService.getUiVocabularies().subscribe(
-      res => {this.vocabularies = res}
-    );
+
+    this.setVocabularies();
+
     if (this.providerBundle) {
       this.provider = this.providerBundle.provider;
     }
@@ -40,5 +56,40 @@ export class ProviderInfoComponent implements OnInit, OnChanges {
         return vocabulary.name;
     }
   }
+
+  setVocabularies() {
+    this.resourceService.getUiVocabularies().subscribe(
+      res => {
+        this.vocabularies = res
+      }
+    );
+  }
+
+  // setVocabularies() {
+  //   this.resourceService.getAllVocabulariesByType().subscribe(
+  //     res => {
+  //       this.vocabularies = res;
+  //       this.placesVocabulary = this.vocabularies[Type.COUNTRY];
+  //       this.providerTypeVocabulary = this.vocabularies[Type.PROVIDER_STRUCTURE_TYPE];
+  //       this.providerLCSVocabulary = this.vocabularies[Type.PROVIDER_LIFE_CYCLE_STATUS];
+  //       this.domainsVocabulary = this.vocabularies[Type.SCIENTIFIC_DOMAIN];
+  //       this.categoriesVocabulary = this.vocabularies[Type.SCIENTIFIC_SUBDOMAIN];
+  //       this.merilDomainsVocabulary = this.vocabularies[Type.PROVIDER_MERIL_SCIENTIFIC_DOMAIN];
+  //       this.merilCategoriesVocabulary = this.vocabularies[Type.PROVIDER_MERIL_SCIENTIFIC_SUBDOMAIN];
+  //       this.esfriDomainVocabulary = this.vocabularies[Type.PROVIDER_ESFRI_DOMAIN];
+  //       this.legalStatusVocabulary = this.vocabularies[Type.PROVIDER_LEGAL_STATUS];
+  //       this.esfriVocabulary = this.vocabularies[Type.PROVIDER_ESFRI_TYPE];
+  //       this.areasOfActivityVocabulary = this.vocabularies[Type.PROVIDER_AREA_OF_ACTIVITY];
+  //       this.networksVocabulary = this.vocabularies[Type.PROVIDER_NETWORK];
+  //       this.societalGrandChallengesVocabulary = this.vocabularies[Type.PROVIDER_SOCIETAL_GRAND_CHALLENGE];
+  //       // this.hostingLegalEntityVocabulary = this.vocabularies[Type.PROVIDER_HOSTING_LEGAL_ENTITY];
+  //       return this.vocabularies;
+  //     },
+  //     error => console.log(JSON.stringify(error.error)),
+  //     () => {
+  //       return this.vocabularies;
+  //     }
+  //   );
+  // }
 
 }
